2b982fa5a05e1b110dbe0bf441e0298a85439e4a
quwala <quwala@gmail.com>
2018-02-12 16:13:24 +0200

    a running hgello world example

--- iot_proj/src/main/java/com/iot_proj/iot_proj/SanityTest.java
+++ iot_proj/src/main/java/com/iot_proj/iot_proj/SanityTest.java
@@ -4,9 +4,11 @@ import il.ac.bgu.cs.bp.bpjs.bprogram.runtimeengine.BProgramRunner;
 import il.ac.bgu.cs.bp.bpjs.bprogram.runtimeengine.BProgram;
 import il.ac.bgu.cs.bp.bpjs.bprogram.runtimeengine.SingleResourceBProgram;
 import il.ac.bgu.cs.bp.bpjs.events.BEvent;
+import il.ac.bgu.cs.bp.bpjs.bprogram.runtimeengine.listeners.InMemoryEventLoggingListener;
 import il.ac.bgu.cs.bp.bpjs.bprogram.runtimeengine.listeners.PrintBProgramRunnerListener;
 import il.ac.bgu.cs.bp.bpjs.eventselection.PrioritizedBSyncEventSelectionStrategy;
 import il.ac.bgu.cs.bp.bpjs.verification.DfsBProgramVerifier;
+import il.ac.bgu.cs.bp.bpjs.verification.VerificationResult;
 import il.ac.bgu.cs.bp.bpjs.verification.VerificationResult;
 
 
@@ -15,8 +17,19 @@ public class SanityTest
     public static void main( String[] args )
     {
 		BProgram bprog = new SingleResourceBProgram("our_resources/examples/hello-world-seq.js");
-//		BProgram bprog = new SingleResourceBProgram("hello-world-seq.js");
-
-    }
+//		bprog.setEventSelectionStrategy(new PrioritizedBSyncEventSelectionStrategy());
+		BProgramRunner runner = new BProgramRunner(bprog);
+		runner.addListener(new PrintBProgramRunnerListener());
+		InMemoryEventLoggingListener eventLogger = new InMemoryEventLoggingListener();
+		runner.addListener(eventLogger);
+		try{
+			runner.start();
+			}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+		
+		
+		}
 }
 
