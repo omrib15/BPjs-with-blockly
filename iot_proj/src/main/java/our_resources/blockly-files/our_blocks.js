4005381786251e7f57cb8c06ad8e99ff6003bafa
quwala <quwala@gmail.com>
2018-02-18 01:33:57 +0200

    added list of bp events

--- iot_proj/src/main/java/our_resources/blockly-files/our_blocks.js
+++ iot_proj/src/main/java/our_resources/blockly-files/our_blocks.js
@@ -1,4 +1,3 @@
-//Blockly.JavaScript = Blockly.Generator.get('JavaScript');
 
 /*
 Blockly.Blocks['bp_event'] = {
@@ -23,12 +22,53 @@ Blockly.defineBlocksWithJsonArray([
         "check": "String"
       }
     ],
-	"output": "BP_EVENT",
-    "colour": "0",
+	"inputsInline": true,
+    "previousStatement": null,
+    "output" : "BP_EVENT",
+    "nextStatement": "BP_EVENT", 
+	"colour": 0,
     "tooltip": "A BP Event"
   },
   
+/*   {
+  "type": "bp_event_of_list",
+  "message0": "BP Event %1",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "NAME",
+      "check": "String"
+    }
+  ],
+  "inputsInline": true,
+  "previousStatement": null,
+  "output" : "BP_EVENT",
+  "nextStatement": "BP_EVENT",
+  "colour": 0,
+  "tooltip": "Use this block if you are manually defining a list of BP Events",
+  "helpUrl": ""
+},
+  */
   
+  {
+  "type": "bp_event_list",
+  "message0": "List of BP Events %1 %2",
+  "args0": [
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_statement",
+      "name": "LIST",
+      "check": "BP_EVENT"
+    }
+  ],
+  "output": "BP_EVENT_LIST",
+  "colour": 5,
+  "tooltip": "A list of BP Events",
+  "helpUrl": ""
+},
+
   {
   "type": "bp_bsync",
   "message0": "Wait %1 Request %2 Block %3",
@@ -36,23 +76,48 @@ Blockly.defineBlocksWithJsonArray([
     {
       "type": "input_value",
       "name": "WAIT",
-      "check": "[BP_EVENT,BP_EVENT_LIST]"
+      "check": ["BP_EVENT","BP_EVENT_LIST"]
     },
     {
       "type": "input_value",
       "name": "REQUEST",
-      "check": "[BP_EVENT,BP_EVENT_LIST]"
+      "check": "BP_EVENT"
     },
     {
       "type": "input_value",
       "name": "BLOCK",
-      "check": "[BP_EVENT,BP_EVENT_LIST]"
+      "check": ["BP_EVENT","BP_EVENT_LIST"]
     }
   ],
   "previousStatement": null,
   "nextStatement": null,
-  "colour": 0,
+  "colour": 5,
   "tooltip": "A single bsync statement",
+  "helpUrl": ""
+},
+{
+  "type": "bp_bsync_with_output",
+  "message0": "Wait %1 Request %2 Block %3",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "WAIT",
+      "check": ["BP_EVENT","BP_EVENT_LIST"]
+    },
+    {
+      "type": "input_value",
+      "name": "REQUEST",
+      "check": "BP_EVENT"
+    },
+    {
+      "type": "input_value",
+      "name": "BLOCK",
+      "check": ["BP_EVENT","BP_EVENT_LIST"]
+    }
+  ],
+  "output":"BP_EVENT",
+  "colour": 5,
+  "tooltip": "Use this block if you would like to utilize the value returned by the bsync",
   "helpUrl": ""
 }
   ])
@@ -70,17 +135,45 @@ Blockly.defineBlocksWithJsonArray([
   
   
   Blockly.JavaScript['bp_event'] = function(block) {
-  var event_name = block.getFieldValue('END') == 'FIRST' ? 'indexOf' : 'lastIndexOf';
-  var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = '...;\n';
-  return code;
-};
+  var event_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
+  if(event_name == '\'\'')
+	  event_name = '\'Anonymous event\''
+  var code = 'bp.Event('+event_name+')';
+  return [code, Blockly.JavaScript.ORDER_ATOMIC]};
+  
+  Blockly.JavaScript['bp_event_of_list'] =  Blockly.JavaScript['bp_event']
+  
+  
+  
+  Blockly.JavaScript['bp_event_list'] = function(block) {
+  var events_string = Blockly.JavaScript.valueToCode(block, 'LIST');
+  console.log(events_string);
+  events = breakEventsString(events_string);
+  var code = '';
+  events.forEach(function(entry){
+	code+=entry+',';
+  });
+  code=code.substring(0,code.length-1);
+  return [code, Blockly.JavaScript.ORDER_ATOMIC]};
+  
+  var breakEventsString = function(events_string){
+	split = events_string.split('bp.Event');
+	result = []
+	split.forEach(function(entry){
+	if(entry.startsWith('(') && entry.endsWith(')'))
+		result.push('bp.Event'+entry)
+	});
+
+	return result
+  };
   
  Blockly.JavaScript['bp_bsync'] = function(block) {
-  var value_wait = Blockly.JavaScript.valueToCode(block, 'WAIT', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_request = Blockly.JavaScript.valueToCode(block, 'REQUEST', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_block = Blockly.JavaScript.valueToCode(block, 'BLOCK', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
+  var value_wait = Blockly.JavaScript.valueToCode(block, 'WAIT', Blockly.JavaScript.ORDER_ATOMIC) || 'null';
+  var value_request = Blockly.JavaScript.valueToCode(block, 'REQUEST', Blockly.JavaScript.ORDER_ATOMIC) || 'null';
+  var value_block = Blockly.JavaScript.valueToCode(block, 'BLOCK', Blockly.JavaScript.ORDER_ATOMIC) || 'null';
+
+  var code = 'bsync({waitFor: '+value_wait+',request: '+value_request+',block: '+value_block+'});';
+  return [code, Blockly.JavaScript.ORDER_ATOMIC];
 };
+
+Blockly.JavaScript['bp_bsync_with_output'] = Blockly.JavaScript['bp_bsync'] //same generation
