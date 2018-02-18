900581bf59bd0ef89618aee57deb13073900ffbf
Omri Basch <omrib15@gmail.com>
2018-02-18 15:40:37 +0200

    create button functionality

--- iot_proj/src/main/java/com/iot_proj/iot_proj/gui/MainFrameFuncs.java
+++ iot_proj/src/main/java/com/iot_proj/iot_proj/gui/MainFrameFuncs.java
@@ -1,9 +1,13 @@
 package com.iot_proj.iot_proj.gui;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintStream;
 
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
+
+import com.iot_proj.iot_proj.blocklyeditor.BlocklyRunner;
 
 import il.ac.bgu.cs.bp.bpjs.execution.BProgramRunner;
 import il.ac.bgu.cs.bp.bpjs.execution.listeners.BProgramRunnerListener;
@@ -15,23 +19,40 @@ import il.ac.bgu.cs.bp.bpjs.model.SingleResourceBProgram;
 
 public class MainFrameFuncs {
 	private PrintStream logStream;
+	private Thread currentRunnerThread;
+	private BlocklyRunner blocklyRunner;
 	
 	public MainFrameFuncs(JTextArea logTextArea) {
-		this.logStream = new PrintStream(new CustomOutStream(logTextArea)); 
+		this.logStream = new PrintStream(new CustomOutStream(logTextArea), true);
+		this.blocklyRunner = new BlocklyRunner();
 	}
 	
 	public void runBprog(String path) throws InterruptedException{
-		//running 
+		
 		BProgram bprog = new SingleResourceBProgram(path);
-		//bprog.setDaemonMode(true);
+		//allow external events
+		bprog.setDaemonMode(true);
 		
 		BProgramRunner runner = new BProgramRunner(bprog);
 		
+		//my custom listener that prints logs to the appropriate text area
 		CustomBProgramRunnerListener eventLogger = new CustomBProgramRunnerListener(logStream);
-	
+		
 		runner.addListener(eventLogger);
 		
-			runner.start();
+		//set the thread that starts the BProgramRunner
+		currentRunnerThread = new Thread(){
+			public void run(){
+				try {
+					runner.start();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		
+		//start the thread
+		currentRunnerThread.start();
 		
                
 	}
@@ -44,6 +65,16 @@ public class MainFrameFuncs {
 	public void setLogStream(PrintStream logStream) {
 		this.logStream = logStream;
 	}
+	
+	public void openBlockly(){
+		try {
+			this.blocklyRunner.run();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
 
 		//shows the main frame
     public static void showFrame(JFrame frame){
